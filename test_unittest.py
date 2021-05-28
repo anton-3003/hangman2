989dd5f4e95281b52d8ffae7ca78ef789cdbafb2
anton-3003 <68605875+anton-3003@users.noreply.github.com>
2021-05-28 16:32:18 +0300

    commited

--- test_unittest.py
+++ test_unittest.py
@@ -1,23 +1,13 @@
+
+# hangman = __import__("hangman.py")
+# word = hangman.word
 import unittest
-# import hangman
-#
-#
-# class TestHideWord(unittest.TestCase):
-#     def test_win_msg(self):
-#         self.assertEqual(hangman.win_msg(), 'Поздравляем, вы выиграли!')
+import hangman
 
-class TestUM(unittest.TestCase):
-    def setUp(self):
-        pass
 
-    def tearDown(self):
-        pass
-
-    def test_numbers_3_4(self):
-        self.assertEqual(3 * 4, 12)
-
-    def test_strings_a_3(self):
-        self.assertEqual('a' * 3, 'aaa')
+class TestGame(unittest.TestCase):
+    def test_win_msg(self):
+        self.assertEqual(hangman.win_msg(), 'Поздравляем, вы выиграли!')
 
 
 if __name__ == '__main__':
